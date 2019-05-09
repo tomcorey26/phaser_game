a92f68461ae748979f4aa7e66d736ee4084cbf2f
tomcorey26 <tomcorey26@gmail.com>
2019-05-08 21:16:14 -0400

    works with node server

--- server/public/js/game.js
+++ server/public/js/game.js
@@ -26,11 +26,11 @@ var score = 0;
 var scoreText;
 
 function preload() {
-  this.load.image("sky", "assets/sky.png");
-  this.load.image("ground", "assets/platform.png");
-  this.load.image("star", "assets/star.png");
-  this.load.image("bomb", "assets/bomb.png");
-  this.load.spritesheet("dude", "assets/dude.png", {
+  this.load.image("sky", " /assets/sky.png");
+  this.load.image("ground", "/assets/platform.png");
+  this.load.image("star", "/assets/star.png");
+  this.load.image("bomb", "/assets/bomb.png");
+  this.load.spritesheet("dude", "/assets/dude.png", {
     frameWidth: 32,
     frameHeight: 48
   });
