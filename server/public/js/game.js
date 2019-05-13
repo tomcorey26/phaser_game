afb78e6f997ee80d7cbe4228c2c3e2d50653b6d2
tomcorey26 <tomcorey26@gmail.com>
2019-05-13 11:22:33 -0400

    part 2 completed

--- server/public/js/game.js
+++ server/public/js/game.js
@@ -12,10 +12,33 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-function preload() {}
+function preload() {
+  this.load.image("star", "assets/star.png");
+}
 
 function create() {
+  var self = this;
   this.socket = io();
+  this.players = this.add.group();
+
+  this.socket.on("currentPlayers", function(players) {
+    Object.keys(players).forEach(function(id) {
+      if (players[id].playerId === self.socket.id) {
+        displayPlayers(self, players[id], "star");
+      }
+    });
+  });
 }
 
 function update() {}
+
+function displayPlayers(self, playerInfo, sprite) {
+  const player = self.add
+    .sprite(playerInfo.x, playerInfo.y, sprite)
+    .setOrigin(0.5, 0.5)
+    .setDisplaySize(53, 40);
+  if (playerInfo.team === "blue") player.setTint(0x0000ff);
+  else player.setTint(0xff0000);
+  player.playerId = playerInfo.playerId;
+  self.players.add(player);
+}
